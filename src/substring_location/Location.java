f851e1121319800279a4b4a33638e31dd6a525ae
Ange TOSSOU <angetossou.dev@gmail.com>
2025-02-11 05:29:20 +0100

    Update code

--- src/substring_location/Location.java
+++ src/substring_location/Location.java
@@ -2,7 +2,7 @@ package substring_location;
 
 public class Location {
 
-    public static int getLocation(String message, String substring) {
+    static int getLocation(String message, String substring) {
 
         return message.indexOf(substring) + 1;
 
